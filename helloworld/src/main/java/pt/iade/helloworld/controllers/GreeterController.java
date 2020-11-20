ce4361246c53cd0948f3f345d087c73afa0aa5e0
Ricardo Manuel Gonçalves Vidigal <rmgvidigal@gmail.com>
2020-11-20 15:20:12 +0000

    tutorial 1

--- helloworld/src/main/java/pt/iade/helloworld/controllers/GreeterController.java
+++ helloworld/src/main/java/pt/iade/helloworld/controllers/GreeterController.java
@@ -1,18 +1,19 @@
 package pt.iade.helloworld.controllers;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping(path="/api/greeter")
-public class GreeterController {
+public class GreeterController {  
     private Logger logger = LoggerFactory.getLogger(GreeterController.class);
-    @GetMapping(path = "{name}",
-    produces= Media.APPLICATION_JSON_VALUE)
-    public String getGreeting(@PathVariable("name") String name) {
-        logger.info("Saying Hello to "+name);
-        return "Hello "+name;
+    @GetMapping(path = "", produces= MediaType.APPLICATION_JSON_VALUE)
+    public String getGreeting() {
+        logger.info("Saying Hello to the world");
+        return "Hello World";
     }
 }
